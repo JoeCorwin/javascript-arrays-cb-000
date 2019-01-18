78dfd9671c2a44acac75b5193e2aa5804d8b106b
Joe Corwin <joe.corwin@gmail.com>
2019-01-18 02:08:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ function addElementToBeginningOfArray(array, element) {
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   var arrays = array.unshift(element)
-  return arrays
+
 
 }
 
