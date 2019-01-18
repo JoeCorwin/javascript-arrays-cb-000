e034546482d7a1beb54878c2397ee87c2864c5b7
Joe Corwin <joe.corwin@gmail.com>
2019-01-18 01:58:21 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
-  return [element...array]
+  return [element, ...array]
 }
