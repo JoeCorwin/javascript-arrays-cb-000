610d93acbd2d7c6f958905f4e581961094be3dff
Joe Corwin <joe.corwin@gmail.com>
2019-01-18 02:34:07 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -34,5 +34,8 @@ function removeElementFromBeginningOfArray(array) {
 function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
   return array
+}
 
+function removeElementFromEndOfArray(array) {
+  return array.shift(-1)
 }
